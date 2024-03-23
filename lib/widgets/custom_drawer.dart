0912510a888b7fc50326d70eab0d0f4ea1f4cd3a
Juan Pablo Sierra Diaz <89560629+jsierradiaz@users.noreply.github.com>
2024-03-22 19:15:08 -0700

    (fix): Modified some logic related to asycn gaps. (#3)

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -3,36 +3,40 @@ import 'package:mobile_login_system_app/screens/login.dart';
 import 'package:mobile_login_system_app/services/auth_service.dart';
 import 'package:mobile_login_system_app/utils/secrets.dart';
 
-class CustomDrawer extends StatelessWidget {
+class CustomDrawer extends StatefulWidget {
   const CustomDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  bool isLoggedOut = false;
 
   Future<void> _logout(BuildContext context) async {
     final realmAppId = Secrets.realmAppId;
     var authService = AuthService(realmAppId);
+    var navigator = Navigator.of(context);
+    var messager = ScaffoldMessenger.of(context);
     try {
       await authService.logout();
+      isLoggedOut = true;
     } catch (e) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Logout Failed'),
-            content: Text('Failed to log out user: $e'),
-            actions: <Widget>[
-              TextButton(
-                child: Text('Close'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
+      isLoggedOut = false;
+    }
+
+    if (isLoggedOut) {
+      navigator.pushReplacement(
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    } else {
+      messager.showSnackBar(
+        const SnackBar(
+          content: Text('An error occurred while logging out'),
+          backgroundColor: Colors.red,
+        ),
       );
     }
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => const LoginScreen()),
-    );
   }
 
   @override
@@ -49,10 +53,14 @@ class CustomDrawer extends StatelessWidget {
           ),
           ListTile(
             title: const Text('Logout'),
-            onTap: () {
-              _logout(context);
-
+            onTap: () async {
+              await _logout(context);
               // Update the state of the app
+              // if (isLoggedOut) {
+              //   Navigator.of(context).pushReplacement(
+              //     MaterialPageRoute(builder: (context) => const LoginScreen()),
+              //   );
+              // }
             },
           ),
         ],
